df439e1f501cf605a4e4897d6d2819db20bb5d46
unknown <pal.bijewar@bhavithatech.com>
2024-12-11 13:21:54 +0530

    added: sections folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-import Blogs from "@/src/screens/Blogs/blogs";
-import Contact from "@/src/screens/Contact/contact";
-import Experience from "@/src/screens/Experience/experience";
-import Hero from "@/src/screens/Hero/hero";
-import Skills from "@/src/screens/Skills/skills";
+import { Blogs, Contact, Experience, Hero, Skills } from "./sections";
 
 
 export default function Home() {
   return (
     <div>
-<Hero></Hero>
-<Skills></Skills>
-<Experience></Experience>
-<Blogs></Blogs>
-<Contact></Contact>
+      <Hero></Hero>
+      <Skills></Skills>
+      <Experience></Experience>
+      <Blogs></Blogs>
+      <Contact></Contact>
     </div>
   );
 }
---
+++ app/sections/index.tsx
@@ -0,0 +1,9 @@
+import Blogs from "@/src/screens/Blogs/blogs";
+import Contact from "@/src/screens/Contact/contact";
+import Experience from "@/src/screens/Experience/experience";
+import Hero from "@/src/screens/Hero/hero";
+import Skills from "@/src/screens/Skills/skills";
+
+export {
+    Blogs, Contact, Experience, Hero, Skills
+}
